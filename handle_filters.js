1019ee5df8eaf85a07cc3be843169272418685e3
Dominik Buczkowski <97897239+DominikBuczkowski@users.noreply.github.com>
2023-02-27 16:36:47 +0100

    Update handle_filters.js

--- handle_filters.js
+++ handle_filters.js
@@ -1,7 +1,4 @@
-
 function get_filters() {
-
-    // var slider_value = slider.noUiSlider.get();
 
     var slider_value = [0,0];
     
@@ -25,63 +22,61 @@ function get_filters() {
     Van = document.getElementById('Van');
 
 
+    var filter_type = [];
+    var filter_gas = [];
     var filters = [];
 
     filters.push(slider_value[0], slider_value[1]);
 
     if (Benzyna.checked === true) {
-        filters.push('Benzyna');
+        filter_gas.push('Benzyna');
     }
     if (Diesel.checked === true) {
-        filters.push('diesel');
+        filter_gas.push('diesel');
     }
     if (Elektryk.checked === true) {
-        filters.push('elektryk');
+        filter_gas.push('elektryk');
     }
 
     if (Kompakt.checked === true) {
-        filters.push('kompakt');
+        filter_type.push('kompakt');
     }
     if (Normalne.checked === true) {
-        filters.push('normalne');
+        filter_type.push('normalne');
     }
     if (Suv.checked === true) {
-        filters.push('suv');
+        filter_type.push('suv');
     }
     if (Sportowe.checked === true) {
-        filters.push('Sportowe');
+        filter_type.push('Sportowe');
     }
     if (Limuzyna.checked === true) {
-        filters.push('limuzyna');
+        filter_type.push('limuzyna');
     }
     if (Van.checked === true) {
-        filters.push('van');
+        filter_type.push('van');
     }
 
-    var statments = [Benzyna.checked === false, Diesel.checked === false,Elektryk.checked === false, Kompakt.checked === false, Normalne.checked === false, Suv.checked === false, Sportowe.checked === false, Limuzyna.checked === false, Van.checked === false];
-
+    var statment_gas = [Benzyna.checked === false, Diesel.checked === false,Elektryk.checked === false];
+    var statment_type = [Kompakt.checked === false, Normalne.checked === false, Suv.checked === false, Sportowe.checked === false, Limuzyna.checked === false, Van.checked === false];
 
     var M31 = document.getElementById('M31');
 
-    if (!statments.includes(false)) {
+    if (!statment_gas.includes(false) && !statment_type.includes(false)) {
         M31.style.display = "block";
     }
 
-    else if (filters.some(r=> M31_value.includes(r))) {
+    else if (
+        (filter_gas.some(r=> M31_value.includes(r)) || !statment_gas.includes(false))
+        &&
+        
+        (filter_type.some(r=> M31_value.includes(r)) || !statment_type.includes(false)
+        )) {
         M31.style.display = "block";
     }
     else {
         M31.style.display = "none";
     }
-    
-
-    // if (!filters.some(r=> M31_value.includes(r))) {
-    //     console.log('false');
-    //     block.style.display = "none";
-    // }
-    // else {
-    //     block.style.display = "block";
-    // }
     
 }
 
